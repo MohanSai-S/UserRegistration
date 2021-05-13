d50310582c8a7927ce6c4f27212b3be4334efaa4
MohanSai-S <sathyanarayanamohansai@gmail.com>
2021-05-13 11:02:33 +0530

    Added : Tested the given email samples manually from the main

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -42,10 +42,10 @@ public class UserRegistration {
      * @description Private Method for validating the user input of email
      *
      */
-    private void emailValidation(){
-        System.out.println("email :");
-        String email = scanner.nextLine();
-        pattern = Pattern.matches("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,4}+)*$",email);
+    private void emailValidation(String email){
+//      System.out.println("email :");
+//      String email = scanner.nextLine();
+        pattern = Pattern.matches("^[a-zA-Z0-9-_+]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,4}+)*$",email);
         if(pattern){
             System.out.println("Entered a valid email");
         }else {
@@ -92,7 +92,25 @@ public class UserRegistration {
      * @param args
      */
     public static void main (String[] args){
-        UserRegistration User = new UserRegistration();
-        User.passwordValidation();
+        UserRegistration user = new UserRegistration();
+        user.emailValidation("abc@yahoo.com");
+        user.emailValidation("abc-100@yahoo.com");
+        user.emailValidation("abc@gmail.com.com");
+        user.emailValidation("abc.100@yahoo.com");
+        user.emailValidation("abc111@abc.com");
+        user.emailValidation("abc-100@abc.net");
+        user.emailValidation("abc.100@abc.com.au");
+        user.emailValidation("abc@1.com");
+        user.emailValidation("abc+100@gmail.com");
+        user.emailValidation("abc");
+        user.emailValidation("abc@.com.my");
+        user.emailValidation("abc123@gmail.a");
+        user.emailValidation("abc123@.com");
+        user.emailValidation(".abc@abc.com");
+        user.emailValidation("abc()*@gmail.com");
+        user.emailValidation("abc@%*.com");
+        user.emailValidation("abc..2002@gmail.com");
+        user.emailValidation("abc.@gmail.com");
+        user.emailValidation("abc@abc@gmail.com");
     }
 }
